2fd162020735baafa63e9061cb7d3dc4987f8b30
Fe23arg <fernando2014argentina@gmail.com>
2023-08-16 16:04:17 -0300

    v1

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ os.environ["OPENBLAS_L2SIZE"]="512k"
 
 app = FastAPI(title = 'Consultas')
 
-df_sub=pd.read_csv('https://console.cloud.google.com/bigquery?ws=!1m5!1m4!4m3!1sarcane-footing-394517!2sProyecto_Final!3sgoogle_concat')
+df_sub=pd.read_csv('https://github.com/Datalogia/ProyectoG_12/raw/main/raw/Gmaps_H/google_concat.parquet')
 #--------------------------------------------------------------------------------------
 @app.get("/")   
 def index():
@@ -30,45 +30,36 @@ def index():
 # Devuelve la distancia desde el punto origen elegido, hasta los puntos
 # o coordenadas de los locales en el dataset estudiado
 
-
 @app.get("/getDistanceBetweenPointsNew/{latitude1}/{longitude1}/{unit}")
 def getDistanceBetweenPointsNew(latitude1:float, longitude1:float,unit:str):
 
 #def rad2deg(radians):
-    degrees = radians * 180 / pi
+  degrees = radians * 180 / pi
 #    return degrees
 
 #def deg2rad(degrees):
-    radians = degrees * pi / 180
+  radians = degrees * pi / 180
 #    return radians
-
-    theta = longitude1 - longitude2
-
-    distance = 60 * 1.1515 * rad2deg(
-        arccos(
+  theta = longitude1 - longitude2
+  distance = 60 * 1.1515 * rad2deg(
+  arccos(
             (sin(deg2rad(latitude1)) * sin(deg2rad(latitude2))) +
             (cos(deg2rad(latitude1)) * cos(deg2rad(latitude2)) * cos(deg2rad(theta)))
         )
     )
 
-    if unit == 'miles':
-        return round(distance, 2)
-    if unit == 'kilometers':
-        return round(distance * 1.609344, 2)
+  if unit == 'miles':
+      distance=round(distance, 2)
+  if unit == 'kilometers':
+      distance=round(distance * 1.609344, 2)
 
-df_sub['distancia']=''
-for i in df_sub.index:
-  latitude2=df_sub.latitude[i]
-  #print(latitude2)
-  longitude2=df_sub.longitude[i]
-  #print(longitude2)
-  
-  #print(d)
-  df_sub.distancia[i]=distance
+  df_sub['distancia']=''
+  for i in df_sub.index:
+      latitude2=df_sub.latitude[i]
+      longitude2=df_sub.longitude[i]
+      df_sub.distancia[i]=distance
 
-df_sub.sort_values('distancia', inplace=True)
-
-
-     
-  return {'recomendadas por prioridad descendente':dfg}
-----------------------------------------------------
+  df_sub.sort_values('distancia', inplace=True)     
+    return {'recomendadas por prioridad descendente':distance}
+#reemplazar distance con df_sub
+#----------------------------------------------------
